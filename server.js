6d506be081ff7faf11248285402d135dffff03ad
erickteckio1211 <89051922+erickteckio1211@users.noreply.github.com>
2026-04-25 01:24:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,37 @@ const app = express();
 
 app.use(express.json());
 
+let ultimosDados = [];
+
 app.get("/", (req, res) => {
-  res.send("Servidor ESP32 funcionando 🚀");
+  let html = `
+    <h1>Dados recebidos do ESP32</h1>
+    <p>Total recebido: ${ultimosDados.length}</p>
+    <pre>${JSON.stringify(ultimosDados, null, 2)}</pre>
+  `;
+
+  res.send(html);
 });
 
 app.post("/dados", (req, res) => {
-  console.log("📩 Dados recebidos:");
-  console.log(req.body);
+  const dados = req.body;
+
+  console.log("Dados recebidos:", dados);
+
+  ultimosDados.push(dados);
+
+  if (ultimosDados.length > 20) {
+    ultimosDados.shift();
+  }
 
   res.json({
-    status: "ok"
+    status: "ok",
+    recebido: dados
   });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
